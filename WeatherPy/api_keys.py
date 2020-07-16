ef66a52e2f9f39f21b31e218c69a78f031617237
Ashish Desai <ashishdesai@MacBook-Pro.local>
2020-07-15 20:57:34 -0500

    Secured api_key

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "5fd121bff57b2f82a5f586ddd82338d4"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyCJ_liD0fWy7_0oqbr84i5jqzhuYOL0cFg"
+g_key = ""
