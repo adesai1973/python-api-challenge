7718e4e659b7ed255d17c335c5a549b79d5e5e15
Ashish Desai <ashishdesai@MacBook-Pro.local>
2020-07-21 12:08:52 -0700

    Added VacationPy Solution

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -2,4 +2,4 @@
 weather_api_key = "5fd121bff57b2f82a5f586ddd82338d4"
 
 # Google API Key
-g_key = "AIzaSyCJ_liD0fWy7_0oqbr84i5jqzhuYOL0cFg"
+g_key = "AIzaSyBavCSByG-34JMm7hkZct_Hd0y3_wF3bf8"
--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = ""
+weather_api_key = "5fd121bff57b2f82a5f586ddd82338d4"
 
 # Google API Key
-g_key = ""
+g_key = "AIzaSyBavCSByG-34JMm7hkZct_Hd0y3_wF3bf8"
